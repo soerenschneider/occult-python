28d8d12ba2c638496ba46ff5a6e6cf8fca77f470
soeren <56670304+soerenschneider@users.noreply.github.com>
2021-09-04 12:24:11 +0200

    feat: ability for token reflection regarding ttl
    Added poor man's metric implementation for communicating soon expiring token

--- occult.py
+++ occult.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import datetime
 import os
 import logging
 import json
@@ -46,6 +46,22 @@ class Context:
             value = value[k]
         return value
 
+    def get_token_ttl(self, token: Dict) -> Optional[int]:
+        content = self._lookup_self(token)
+        if "ttl" in content["data"]:
+            return content["data"]["ttl"]
+        return None
+
+    @backoff.on_exception(backoff.expo, requests.exceptions.RequestException)
+    def _lookup_self(self, token: str) -> Dict:
+        logging.info("Trying to lookup used token")
+        url = urljoin(self._endpoint, f"/v1/auth/token/lookup-self")
+        resp = requests.get(headers={'X-Vault-Token': token}, url=url)
+        if resp.status_code > 204:
+            raise VaultException(f"Couldn't lookup token, got HTTP {resp.status_code}: {resp.content} for {url}")
+
+        return json.loads(resp.content)
+
     @backoff.on_exception(backoff.expo, requests.exceptions.RequestException)
     def read_pass(self, vault_secret_path: str, token: str, json_secret_path: str) -> Optional[str]:
         logging.info("Trying to read secret '%s'", vault_secret_path)
@@ -90,7 +106,20 @@ def validate_config(config):
 
     if "token" not in config:
         if "role_id" not in config and "secret_id" not in config:
-            raise ConfigError(f"either specify 'token' or 'secret_id' and 'role_id' values")
+            raise ConfigError("either specify 'token' or 'secret_id' and 'role_id' values")
+
+
+def write_metrics_file(metrics_file: str, token_ttl: int, success: bool) -> None:
+    # instead of adding another dependency, we just write this simple metrics file manually
+    payload = f"""# TYPE occult_token_ttl_seconds gauge
+occult_token_ttl_seconds { token_ttl }
+# TYPE occult_last_invocation_seconds gauge
+occult_last_invocation_seconds { datetime.datetime.now().timestamp() }
+# TYPE occult_success_bool gauge
+occult_success_bool { 1 if success else 0 }"""
+
+    with open(metrics_file, 'w') as f:
+        f.write(payload)
 
 
 def main(config_file: str) -> None:
@@ -109,6 +138,8 @@ def main(config_file: str) -> None:
         logging.error("Invalid config: %s", err)
         sys.exit(1)
 
+    success = False
+    ttl = -1
     try:
         ctx = Context(conf)
         token = None
@@ -117,24 +148,35 @@ def main(config_file: str) -> None:
         else:
             token = ctx.authenticate()
 
+        ttl = ctx.get_token_ttl(token)
+        logging.info("Token expires in %d seconds (on %s)", ttl, datetime.datetime.now() + datetime.timedelta(seconds=ttl))
+
         json_secret_path = DEFAULT_JSON_SECRET_PATH
         if "json_secret_path" in conf:
             json_secret_path = conf["json_secret_path"]
         password = ctx.read_pass(conf["path"], token, json_secret_path)
+
         ctx.send_password(password)
         if "post_hook" in conf and len(conf["post_hook"]) > 0:
             ctx.post_hook()
+
+        success = True
     except KeyError as err:
         logging.error("No such field found in reply from vault, check json_secret_path: %s", err)
-        sys.exit(1)
     except VaultException as err:
         logging.error("Error talking to vault: %s", err)
-        sys.exit(1)
     except FileNotFoundError as err:
         logging.error("No such cmd: %s", err)
-        sys.exit(1)
     except CmdNotSuccessfulException:
         logging.error("Command unsuccessful")
+
+    if "metrics_file" in conf:
+        logging.info("Writing metrics to %s", conf["metrics_file"])
+        write_metrics_file(conf["metrics_file"], ttl, success)
+    else:
+        logging.warning("Not writing metrics, no metrics_file specified")
+
+    if not success:
         sys.exit(1)
 
 
